4345e4170bfc9f059fc7ee70279c0e54c5442746
HwangWonGyu <hwksdream@naver.com>
2021-01-05 17:35:40 +0900

    project creation

---
+++ src/main/java/com/hwangwongyu/webbulletinboardstudy/WebBulletinBoardStudyApplication.java
@@ -0,0 +1,13 @@
+package com.hwangwongyu.webbulletinboardstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBulletinBoardStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBulletinBoardStudyApplication.class, args);
+	}
+
+}
