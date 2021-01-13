df8031e69ca3ac992f068296a32a682f7b4a60aa
HwangWonGyu <hwksdream@naver.com>
2021-01-13 17:49:30 +0900

    Change test class name for project name changing

---
+++ src/test/java/com/hwangwongyu/news/NewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwangwongyu.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
