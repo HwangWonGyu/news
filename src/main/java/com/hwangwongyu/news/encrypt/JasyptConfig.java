5f3d350d31a9b8486b18cf8499038358b7325e88
HwangWonGyu <hwksdream@naver.com>
2021-01-14 19:49:26 +0900

    Encrypt database account information

---
+++ src/main/java/com/hwangwongyu/news/encrypt/JasyptConfig.java
@@ -0,0 +1,21 @@
+package com.hwangwongyu.news.encrypt;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword("wgh");
+        config.setPoolSize(1);
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
