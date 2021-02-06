bfad096ecce885edc3ce9eb0a8724fee8ac6d95a
HwangWonGyu <hwksdream@naver.com>
2021-02-07 04:09:38 +0900

    Change session info class's wiring annotation

--- src/main/java/com/hwangwongyu/news/login/LoginController.java
+++ src/main/java/com/hwangwongyu/news/login/LoginController.java
@@ -12,12 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-
 @RestController
 public class LoginController {
 
-    @Resource
+    @Autowired
     private UserLoginInfo userLoginInfo;
 
     @Autowired
