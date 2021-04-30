afd2b904cf55669134686c888e708ca88a808360
HwangWonGyu <hwksdream@naver.com>
2021-04-30 15:34:58 +0900

    Add comments for Unit Test description and reference posting link

--- src/test/java/com/hwangwongyu/news/user/UserCreateTest.java
+++ src/test/java/com/hwangwongyu/news/user/UserCreateTest.java
@@ -18,6 +18,11 @@ import static org.mockito.BDDMockito.given;
 /*
  JUnit : 자바와 JVM 계열의 언어(예 : 코틀린)에서 사용하는 단위 테스트 프레임워크를 말한다.
 
+ 단위 테스트(Unit Test) : 소스코드의 특정 모듈(프로그램 내 하나의 기능을 부르는 말)이 의도된 대로 정확히 작동하는지 검증하는 절차이며,
+ 함수, 메서드, 개별 코드 같은 작은 단위에 대해 테스트 케이스(Test Case)로 분리하고 테스트 코드를 작성하여 테스트하는 것을 말한다.
+ 외부 API와의 연동이 필수라든가 DB 데이터 접근 등 외부 리소스를 직접 사용해야 하는 테스트라면 단위 테스트가 아니다.
+ 단위 테스트에서 외부와의 연동이 필요하다면 테스트 대역(Test Double)을 사용하면 된다.
+
  Mockito : 단위 테스트를 위한 자바 Mocking 프레임워크 중 하나이다.
  테스트 대역(Test Double)의 종류 중 모의(Mock) 객체를 필요로 할 때 사용한다.
 
@@ -25,6 +30,15 @@ import static org.mockito.BDDMockito.given;
 
  모의(Mock) 객체 : 호출했을 때 사전에 정의된 명세대로의 결과를 돌려주도록 미리 프로그램 돼있는 것이다.
  예상치 못한 호출이 있을 경우 예외를 던질 수 있으며 모든 호출이 예상된 것이었는지 확인할 수 있다.
+
+ JUnit의 개념과 장점, 단위 테스트의 개념과 장점, 단위/통합/기능 테스트
+ https://beststar-1.tistory.com/27
+
+ 테스트 대역(Test Double)
+ https://beststar-1.tistory.com/29
+
+ Mockito - Mockito란, Stubbing/Verification, Mock 객체 생성 방법과 동작 원리
+ https://beststar-1.tistory.com/30
  */
 
 @ExtendWith(MockitoExtension.class)
